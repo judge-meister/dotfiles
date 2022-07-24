00972320e92138127f030e7ac3a665d1e1be334a
Ian Judge <ianjudge1969@gmail.com>
2022-07-24 21:18:27 +0100

    updated sway scripts

---
+++ .config/sway/scripts/mpv_wallpaper.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# ~/Videos/lexa-sorted.m3u
+# or given video file
+PLAYLIST=$HOME/Videos/lexa-sorted.m3u
+OUTPUT=LVDS-1
+
+case $1 in
+  start) shift;;
+  quit) random_swaybg.py $HOME/.config/sway/wallpaper.png; 
+        pkill mpvpaper;
+        swaymsg mode default;
+        exit;;
+  pause|play) echo 'cycle pause' | socat - "/tmp/mpv-socket-$OUTPUT"; exit;;
+  mute|unmute) echo 'cycle mute' | socat - "/tmp/mpv-socket-$OUTPUT"; exit;;
+  fore5) echo 'keypress right' | socat - "/tmp/mpv-socket-$OUTPUT"; exit;;
+  back5) echo 'keypress left' | socat - "/tmp/mpv-socket-$OUTPUT"; exit;;
+  fore60) echo 'keypress up' | socat - "/tmp/mpv-socket-$OUTPUT"; exit;;
+  back60) echo 'keypress down' | socat - "/tmp/mpv-socket-$OUTPUT"; exit;;
+  speedup) echo 'keypress ]' | socat - "/tmp/mpv-socket-$OUTPUT"; exit;;
+  slowdown) echo 'keypress [' | socat - "/tmp/mpv-socket-$OUTPUT"; exit;;
+  info) echo 'keypress i' | socat - "/tmp/mpv-socket-$OUTPUT"; exit;;
+  osd) echo 'keypress o' | socat - "/tmp/mpv-socket-$OUTPUT"; exit;;
+esac
+
+if [ $# -gt 0 ]
+then
+  PLAYLIST="$@"
+fi
+
+echo $(pgrep swaybg)|xargs kill
+echo $(pgrep mpvpaper)|xargs kill
+
+mpvpaper -p -o "--loop-playlist shuffle input-ipc-server=/tmp/mpv-socket-$OUTPUT" $OUTPUT "$PLAYLIST"
+
--- .config/sway/scripts/sway_scale.py
+++ .config/sway/scripts/sway_scale.py
@@ -2,22 +2,51 @@
 """script to adjust the screen scaling for a sway window manager"""
 # Requires: sway
 
+import os
 import sys
 import getopt
 import json
 from subprocess import check_output, CalledProcessError
 
+HOME = os.environ["HOME"]
+SCALERC = f"{HOME}/.config/sway/.scale"
+
+def remember_scale(scale):
+    """record scale in hidden file"""
+    with open(SCALERC, 'w') as sc:
+        sc.write(f"{scale:.2f}")
+
+
+def read_scale():
+    """read the remembered scale"""
+    try:
+        with open(SCALERC, 'r') as sc:
+            try:
+                scale=float(sc.read())
+            except ValueError:
+                scale=0.85
+    except FileNotFoundError:
+        scale=0.85
+    return scale
 
 
 def decrease(name, scale):
     """decrease the scaling"""
     scale = min(scale+0.05, 1.0)
+    remember_scale(scale)
     call_swaymsg(name, f"{scale:.2f}")
 
 
 def increase(name, scale):
     """increase the scaling"""
     scale = max(scale-0.05, 0.5)
+    remember_scale(scale)
+    call_swaymsg(name, f"{scale:.2f}")
+
+
+def set_scale(name, scale):
+    """set the screen scale"""
+    remember_scale(scale)
     call_swaymsg(name, f"{scale:.2f}")
 
 
@@ -32,7 +61,11 @@ def call_swaymsg(name, scale): # swaymsg 'output LVDS-1 scale 0.80'
 
 def usage():
     """print usage"""
-    print("Usage: scale_sway.py [-h|-s|-i|-d]")
+    print("Usage: scale_sway.py [-h|-s|-r|-i|-d]")
+    print(" -h    help/usage")
+    print(" -r    report the scale")
+    print(" -i|-d increment or decrement the scale")
+    print(" -s    set the scale")
 
 
 def main():
@@ -43,16 +76,27 @@ def main():
     native = [int(joutput[0]['current_mode']['width']), int(joutput[0]['current_mode']['height'])]
     scaled = [int(native[0]/float(scale)), int(native[1]/float(scale))]
 
-    opts, _args = getopt.getopt(sys.argv[1:], "hsdi", ["help", "scaled", "dec", "inc"])
+    opts, _args = getopt.getopt(sys.argv[1:], "hs:rdi", ["help", "default", "scale=", "report", "dec", "inc"])
 
-    for opt, _a in opts:
+    for opt, a in opts:
         if opt in ['-h', '--help']:
             usage()
             sys.exit()
 
-        elif opt in ['-s', '--scale']:
+        elif opt == "--default":
+            set_scale(name, read_scale())
+
+        elif opt in ['-r', '--report']:
             print(f"{scaled[0]}x{scaled[1]}({scale:.2f})")
 
+        elif opt in ['-s', '--scale']:
+           try:
+               val=float(a)
+               if val >= 0.0 and val <= 1.0:
+                   print(f"call_swaymsg({name},{val})")
+                   set_scale(name, val)
+           except ValueError:
+               sys.exit()
         elif opt in ['-d', '--dec']:
             decrease(name, scale)
 
