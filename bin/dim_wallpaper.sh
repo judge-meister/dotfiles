fdbc153a644a570475b9c9cd1db8dd885d42859a
Ian Judge <ianjudge1969@gmail.com>
2022-07-25 23:03:15 +0100

    proper param handling

--- bin/dim_wallpaper.sh
+++ bin/dim_wallpaper.sh
@@ -1,37 +1,71 @@
 #!/bin/bash
-set -x
-pictures=/home/judge/Pictures/WP
+#set -x
+pictures=/home/judge/clones/wallpapers
 
 opacity=0.65
-shade=out.png
-input=sexy-african-pierced-nipples.png 
-dimensions=$(identify "$pictures"/"$input" |sed  's/.* \([0-9x]*\) .*/\1/g') 
-result=result.png
+shade=/tmp/out.png
+input=${pictures}/boobs/sexy-african-pierced-nipples.png 
+dimensions=$(identify "$input" |sed  's/.* \([0-9x]*\) .*/\1/g') 
+result=$HOME/.config/sway/wallpapers/wallpaper_$$.png
+wallpaper=0
 
 dimmer()
 {
-  rm -f $shade $result
+  rm -f "$shade" "$result"
 
   # create a black image with opacity
-  convert -size "$dimensions" xc:'rgba(0, 0, 0, '$opacity')' $pictures/$shade
+  if ! convert -size "$dimensions" xc:'rgba(0, 0, 0, '$opacity')' "$shade"
+  then
+    echo "Problem with convert command."
+    exit 1
+  fi
 
   # overlay the transparent image onto the wallpaper image
   #convert $input -gravity center $shade -composite $result
-  composite $pictures/$shade $pictures/$input $pictures/$result
-
-  #rm -f $shade
-
-  pid=$(pgrep swaybg)
-  swaybg -m fill -i $pictures/$result 2>/dev/null &
-  kill "$pid"
+  if ! composite "$shade" "$input" "$result"
+  then
+    echo "Problem with composite command."
+    exit 1
+  fi
 }
 
-if [ "$#" -eq 2 ]
-then
-    opacity="$1"
-    input="$2"
-    dimensions=$(identify "$pictures"/"$input" |sed  's/.* \([0-9x]*\) .*/\1/g')
-    dimmer
-else
-    dimmer
-fi
+set_wallpaper()
+{
+  #rm -f $shade
+  if [ $wallpaper -eq 1 ]
+  then
+    cd ~/.config/sway/
+    ln -sf "$result" wallpaper.png
+    pid=$(pgrep swaybg)
+    swaybg -m fill -i "wallpaper.png" 2>/dev/null &
+    echo "$pid" | xargs kill
+  fi
+}
+
+# -i input  -o output  -t transparency  -w (set as new wallpaper)
+usage()
+{
+    echo "dim_wallpaper -i input.jpg -o output.jpg -t opacity  -w (set wallpaper)  -h"
+}
+
+
+while getopts "i:o:t:wh" OPTION; do
+    case $OPTION in
+        i) input="$OPTARG" ;;
+        o) result="$OPTARG" ;;
+        t) opacity="$OPTARG" ;;
+        w) wallpaper=1 ;;
+        h) usage; exit 2 ;;
+        *) echo "Invalid Option [$OPTION]"; usage; exit 2 ;;
+    esac
+done
+dimensions=$(identify "$input" |sed  's/.* \([0-9x]*\) .*/\1/g')
+
+#echo "$input $result $opacity $wallpaper"
+#echo $dimensions
+
+#exit
+
+dimmer
+set_wallpaper
+
