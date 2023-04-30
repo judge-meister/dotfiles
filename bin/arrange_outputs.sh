2d6b886ee8a484acba262a712b44e734d73ffe5f
Ian Judge <ianjudge1969@gmail.com>
2023-04-30 16:57:22 +0100

    question left and right reversed

--- bin/arrange_outputs.sh
+++ bin/arrange_outputs.sh
@@ -18,23 +18,28 @@ function output_options()
 function horizontal_output()
 {
   
-    echo "Horizontal ${OUTPUTS[$1]} ${OUTPUT[$2]}"
+    echo "Horizontal ${OUTPUTS[$1]} ${OUTPUTS[$2]}"
     WIDTH=$(swaymsg -t get_outputs -r | jq '.['$1'].rect.width')
+    #set -x
     swaymsg output ${OUTPUTS[$1]} pos 0 0
     swaymsg output ${OUTPUTS[$2]} pos $WIDTH 0
+    #set +x
 }
 
 function vertical_output()
 {
     echo "Vertical ${OUTPUTS[$1]} ${OUTPUTS[$2]}"
     HEIGHT=$(swaymsg -t get_outputs -r | jq '.['$1'].rect.height')
+    #set -x
     swaymsg output ${OUTPUTS[$1]} pos 0 0
     swaymsg output ${OUTPUTS[$2]} pos 0 $HEIGHT 
+    #set +x
 }
 
 if [ "${#OUTPUTS[@]}" -eq 1 ]
 then
   echo "Single output only."
+  swaymsg output ${OUTPUTS[$1]} pos 0 0
   exit
 fi
 
@@ -43,10 +48,10 @@ read ans
 if [ "${ans,,}" == "h" ]
 then
   ORIENTATION="horiz"
-  echo -e "Which display to the right ? "
+  echo -e "Which display to the left ? "
   output_options
   read first
-  echo -e "Which display to the left ? "
+  echo -e "Which display to the right ? "
   output_options
   read second
   horizontal_output $((first-1)) $((second-1))
